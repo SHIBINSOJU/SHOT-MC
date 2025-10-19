55ac1ba2ac6964ba624b0999cfdd5b04b5fea08e
Shibin Hussain MK <160138800+SHIBINSOJU@users.noreply.github.com>
2025-10-19 09:25:27 +0530

    Update help command to use embeds for command list
    Refactor help command to use EmbedBuilder for response.

--- commands/help.js
+++ commands/help.js
@@ -1,19 +1,30 @@
-
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
         .setDescription('Lists all available commands.'),
     async execute(interaction) {
+        // This line gets all the commands you've loaded
         const commands = interaction.client.commands;
+
+        // This creates the list of commands, just like before
         const commandList = commands.map(command => {
             return `\`/${command.data.name}\` - ${command.data.description}`;
         }).join('\n');
 
+        // Create the new embed
+        const helpEmbed = new EmbedBuilder()
+            .setColor(0x5865F2) // A nice Discord blue/purple color
+            .setTitle('Help Menu')
+            .setDescription(commandList) // The list of commands is the main body
+            .setTimestamp()
+            .setFooter({ text: `Requested by ${interaction.user.username}` });
+
+        // Reply with the embed
         await interaction.reply({
-            content: `**Available Commands:**\n${commandList}`,
-            ephemeral: true,
+            embeds: [helpEmbed], // You send embeds in an array
+            ephemeral: true, // This keeps the message visible only to the user
         });
     },
 };
